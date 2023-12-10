e1a199f707bfaf58a9c13982586bd0c14fd1b578
Ash Kulkarni <ashaykulkarni@outlook.com>
2023-12-10 16:19:52 +0000

    day 6 part 1

---
+++ day-6/src/lib.rs
@@ -0,0 +1,47 @@
+pub fn process_part_1(input: &str) -> i32 {
+    let data: Vec<Vec<i32>> = input.lines().map(|line| {
+        line.split(":").nth(1).unwrap()
+            .split_whitespace().map(|n| n.parse::<i32>().unwrap()).collect()
+    }).collect();
+
+    let times = &data[0];
+    let distances = &data[1];
+
+    times.iter().zip(distances.iter()).map(|(&time, &distance_to_beat)| {
+        let possible_distances = get_possible_distances(time);
+        possible_distances.iter().filter(|&&d| d>distance_to_beat).count() as i32
+
+    }).product()
+}
+
+fn get_possible_distances(time: i32) -> Vec<i32> {
+    let mut possible_distances = Vec::new();
+    for t in 1..=time {
+        possible_distances.push(t*(time-t));
+    }
+    possible_distances
+}
+
+pub fn process_part_2(input: &str) -> i32 {
+    8
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works_1() {
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        let result = process_part_1(input);
+        assert_eq!(result, 288);
+    }
+
+    // #[test]
+    // fn it_works_2() {
+    //     let input = "";
+    //     let result = process_part_2(input);
+    //     assert_eq!(result, 46);
+    // }
+}
